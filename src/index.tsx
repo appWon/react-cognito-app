1d4c5967c24472a81521bf717988158d6f416dea
jaewon <korkrip11@gmail.com>
2021-10-08 23:45:51 +0900

    update aws amplify appsync

--- src/index.tsx
+++ src/index.tsx
@@ -8,19 +8,23 @@ import { Amplify } from "aws-amplify";
 import { config } from "./lib/config";
 
 Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
   Storage: {
     region: config.s3.REGION,
     bucket: config.s3.BUCKET,
     identityPoolId: config.cognito.IDENTITY_POOL_ID,
   },
   API: {
-    endpoints: [
-      {
-        name: "video",
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION,
-      },
-    ],
+    aws_appsync_graphqlEndpoint:
+      "https://lvfcpc6oqza4nl3pcsnstbuj2m.appsync-api.ap-northeast-2.amazonaws.com/graphql",
+    aws_appsync_region: config.cognito.REGION,
+    aws_appsync_authenticationType: "AWS_IAM",
   },
 });
 
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,14 +3,15 @@ export const config = {
     REGION: process.env.REACT_APP_S3_REGION,
     BUCKET: process.env.REACT_APP_S3_BUCKET_NAME,
   },
-  apiGateway: {
-    URL: process.env.REACT_APP_GATE_WAY_URL,
-    REGION: process.env.REACT_APP_GATE_WAY_REGION,
-  },
   cognito: {
     REGION: process.env.REACT_APP_REGION,
     USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
     APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
     IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL,
+  },
+  appSync: {
+    endPoint: process.env.REACT_APP_END_POINT,
+    region: process.env.REACT_APP_APP_SYNC_REGION,
+    appSyncAuthType: process.env.REACT_APP_APP_SYNC_AUTH_TYPE,
   },
 };
